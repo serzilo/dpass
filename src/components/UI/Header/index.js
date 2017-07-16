3fc2868bf548d7ec4a5e87dd0e2d20e4e141df76
Sergey Shibalov <sergey.shibalov@swisstok.ru>
2017-07-16 11:28:56 +0300

    links config

--- src/components/UI/Header/index.js
+++ src/components/UI/Header/index.js
@@ -1,6 +1,8 @@
 import React, { PropTypes, Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import links from '../../../configs/links.json';
+
 import cx from 'classnames';
 
 export default class Header extends Component {
@@ -9,15 +11,15 @@ export default class Header extends Component {
             <header>
                 <nav>
                     <ul>
-                        <li><NavLink to="/" exact={true} activeClassName="selected">Home</NavLink></li>
-                        <li><NavLink to="/ask" activeClassName="selected">Поддержка</NavLink></li>
-                        <li><NavLink to="/download" activeClassName="selected">Скачать</NavLink></li>
-                        <li><NavLink to="/prices" activeClassName="selected">Цены</NavLink></li>
+                        <li><NavLink to={links.home} exact={true} activeClassName="selected">Home</NavLink></li>
+                        <li><NavLink to={links.ask} activeClassName="selected">Поддержка</NavLink></li>
+                        <li><NavLink to={links.download} activeClassName="selected">Скачать</NavLink></li>
+                        <li><NavLink to={links.prices} activeClassName="selected">Цены</NavLink></li>
                     </ul>
 
                     <ul>
-                        <li><NavLink to="/registration" activeClassName="selected">Подключить</NavLink></li>
-                        <li><NavLink to="/login" activeClassName="selected">Войти</NavLink></li>
+                        <li><NavLink to={links.registration} activeClassName="selected">Подключить</NavLink></li>
+                        <li><NavLink to={links.login} activeClassName="selected">Войти</NavLink></li>
                     </ul>
                 </nav>
             </header>
